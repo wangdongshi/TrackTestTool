abbd28cee42c358ddc3a5f8d0c2312b10e523e27
wangyu <ouu_cn@163.com>
2023-07-07 19:47:27 +0800

    ADC7608 module can output data continuously.

--- Src/ad7608.c
+++ Src/ad7608.c
@@ -8,70 +8,68 @@
  * Author   : WangYu
  *
  **********************************************************************/
+ 
+/* Includes ------------------------------------------------------------------*/
 #include "debug.h"
 #include "stm32f4xx_hal.h"
 #include "cmsis_os.h"
 
-#if 0
-#define AD7608OS0_H LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_13)
-#define AD7608OS0_L LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_13)
-#define AD7608OS1_H LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_14)
-#define AD7608OS1_L LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_14)
-#define AD7608OS2_H LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_15)
-#define AD7608OS2_L LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_15)
+#if 1
 
-#define AD7608_CONVST_A_H LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_1)
-#define AD7608_CONVST_A_L LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_1)
-#define AD7608_CONVST_B_H LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_2)
-#define AD7608_CONVST_B_L LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_2)
+/* Private macro -------------------------------------------------------------*/
 
-#define AD7608_SCLK_H LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_0)
-#define AD7608_SCLK_L LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_0)
-#define AD7608_RESET_H LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_1)
-#define AD7608_RESET_L LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_1)
-#define AD7608_CS_H LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_2)
-#define AD7608_CS_L LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_2)
+/* STA  : PC04
+ * STB  : short to PC04
+ * SCLK : PA05
+ * RST  : PA07
+ * CS   : PA04
+ * BUSY : PC05
+ * DOA  : PA06
+ */
 
-#define AD7608_BUSY GPIO_ReadInputDataBit(GPIOA, LL_GPIO_PIN_3)
-#define AD7608_DOUTA GPIO_ReadInputDataBit(GPIOA, LL_GPIO_PIN_4)
-#define AD7608_DOUTB GPIO_ReadInputDataBit(GPIOA, LL_GPIO_PIN_5)
+/* Output definition */
+#define AD7608_CONVSTA_H   LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_4)
+#define AD7608_CONVSTA_L LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_4)
+#define AD7608_SCLK_H      LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_5)
+#define AD7608_SCLK_L    LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_5)
+#define AD7608_RESET_H     LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_7)
+#define AD7608_RESET_L   LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_7)
+#define AD7608_CS_H        LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_4)
+#define AD7608_CS_L      LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_4)
 
-//#define AD7608_SYNC_H LL_GPIO_SetOutputPin(GPIOA, GPIO_Pin_6)
-//#define AD7608_SYNC_L LL_GPIO_ResetOutputPin(GPIOA, GPIO_Pin_6)
+#define AD7608_BUSY       LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_5)
+#define AD7608_DOUTA      LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_6)
 
-extern UART_HandleTypeDef huart1;
+/* Private Variables ---------------------------------------------------------*/
+uint32_t adc_data[8]; // data length = 18 bit
 
-void RCC_Configuration(void); 
+/* Private function prototypes -----------------------------------------------*/
 void GPIO_Configuration(void);
-void NVIC_Configuration(void); 
-void Delay(uint32_t nCount);
 void AD7608_SETOS(uint8_t osv);
 void AD7608_RESET(void);
-uint32_t ad7606_ReadDATA(void);
 void AD7608_STARTCONV(void);
+void Delay(uint32_t nCount);
+uint32_t AD7608_READDATA(void);
 
-
-uint32_t datatemp[8];
-
+/* Formal function definitions -----------------------------------------------*/
 int ADC_Task(void) 
 {
-  // initialize AD7608
-  RCC_Configuration();
+  // initialize AD7608 GPIO
   GPIO_Configuration();
-  NVIC_Configuration();
 
-  AD7608_SETOS(0X00);
   AD7608_RESET();
-  AD7608_CONVST_A_H;
-  AD7608_CONVST_B_H;
+  
+  AD7608_CONVSTA_H;
   
   // trigger ADC transfer and read data 
   while (1) {
     if(!AD7608_BUSY) {
       AD7608_CS_L;
       for(uint8_t i = 0; i < 8; i++) {
-        datatemp[i] = ad7606_ReadDATA();
-        HAL_UART_Transmit(&huart1, "CH%d : %d\r\n", i, datatemp[i]);
+        adc_data[i] = AD7608_READDATA();
+        PRINTF("CH%d : %d, \t", i, adc_data[i]);
+        if (i == 3 || i == 7) PRINTF("\r\n");
+        if (i == 7) PRINTF("\r\n");
       }
       AD7608_CS_H;
       osDelay(2000);
@@ -79,28 +77,22 @@ int ADC_Task(void)
       while(AD7608_BUSY);
     }
   } 
-} 
+}
 
 void AD7608_STARTCONV(void)
 {
-  AD7608_CONVST_A_L;
-  AD7608_CONVST_B_L;
+  AD7608_CONVSTA_L;
   Delay(0xF);
-  AD7608_CONVST_A_H;
-  AD7608_CONVST_B_H;
+  AD7608_CONVSTA_H;
 }
 
-uint32_t ad7606_ReadDATA(void)
+uint32_t AD7608_READDATA(void)
 {
   uint32_t usData = 0;
-  for (uint8_t i = 0; i < 18; i++)
-  {
+  for (uint8_t i = 0; i < 18; i++) {
     AD7608_SCLK_L;
     usData = usData << 1;
-    if(AD7608_DOUTA)
-    {
-      usData |= 0x0001;
-    }
+    if(AD7608_DOUTA) usData |= 0x0001;
     AD7608_SCLK_H;
   }
   return usData;		
@@ -113,113 +105,63 @@ void AD7608_RESET(void)
   AD7608_RESET_L;
 }
 
-void AD7608_SETOS(uint8_t osv)
+void GPIO_Configuration(void) 
 {
-  switch(osv)
-  {
-  case 0://000
-    AD7608OS0_L;
-    AD7608OS1_L;
-    AD7608OS2_L;
-    break;
-  case 1://001
-    AD7608OS0_H;
-    AD7608OS1_L;
-    AD7608OS2_L;
-    break;
-  case 2://010
-    AD7608OS0_L;
-    AD7608OS1_H;
-    AD7608OS2_L;
-    break;
-  case 3://011
-    AD7608OS0_H;
-    AD7608OS1_H;
-    AD7608OS2_L;
-    break;
-  case 4://100
-    AD7608OS0_L;
-    AD7608OS1_L;
-    AD7608OS2_H;
-    break;
-  case 5://101
-    AD7608OS0_H;
-    AD7608OS1_L;
-    AD7608OS2_H;
-    break;
-  case 6://110
-    AD7608OS0_L;
-    AD7608OS1_H;
-    AD7608OS2_H;
-    break;
-  }
+  LL_GPIO_InitTypeDef GPIO_InitStructure;
+  
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+  
+  /* STA  : PC04
+   * STB  : short to PC04
+   * SCLK : PA05
+   * RST  : PA07
+   * CS   : PA04
+   * BUSY : PC05
+   * DOA  : PA06
+   */
+  
+  /* Set output I/O */
+  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_4);
+  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_5);
+  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_7);
+  LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_4);
+  
+  GPIO_InitStructure.Mode = LL_GPIO_MODE_OUTPUT;
+  
+  GPIO_InitStructure.Pin = LL_GPIO_PIN_4 | LL_GPIO_PIN_5 | LL_GPIO_PIN_7;
+  GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStructure.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStructure.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  GPIO_InitStructure.Pin = LL_GPIO_PIN_4;
+  LL_GPIO_Init(GPIOC, &GPIO_InitStructure);
+  
+  /* Set input I/O */
+  GPIO_InitStructure.Mode = LL_GPIO_MODE_INPUT;
+  
+  GPIO_InitStructure.Pin = LL_GPIO_PIN_6;
+  GPIO_InitStructure.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  GPIO_InitStructure.Pin = LL_GPIO_PIN_5;
+  LL_GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
-void RCC_Configuration(void) 
-{    
-  ErrorStatus HSEStartUpStatus;
-  RCC_DeInit(); 
-  RCC_HSEConfig(RCC_HSE_ON); 
-  HSEStartUpStatus = RCC_WaitForHSEStartUp(); 
-  if(HSEStartUpStatus == SUCCESS) 
-  { 
-    FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable); 
-    FLASH_SetLatency(FLASH_Latency_2); 
-    RCC_HCLKConfig(RCC_SYSCLK_Div1);  
-    RCC_PCLK2Config(RCC_HCLK_Div1);  
-    RCC_PCLK1Config(RCC_HCLK_Div2); 
-    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9); 
-    RCC_PLLCmd(ENABLE); 
-    while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET) { } 
-    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK); 
-    while(RCC_GetSYSCLKSource() != 0x08) { } 
-  } 
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA2, ENABLE); 
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOC| RCC_APB2Periph_GPIOA, ENABLE); 
-} 
-
-void GPIO_Configuration(void) 
-{ 
-  GPIO_InitTypeDef GPIO_InitStructure; 
-  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_11|GPIO_Pin_12; 
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 
-  GPIO_Init(GPIOA, &GPIO_InitStructure); 
-  
-  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15; 
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 
-  GPIO_Init(GPIOA, &GPIO_InitStructure); 
-  
-  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5; 
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
-  GPIO_Init(GPIOA, &GPIO_InitStructure); 
-} 
-
-
-void NVIC_Configuration(void) 
-{ 
-#ifdef  VECT_TAB_RAM   
-  NVIC_SetVectorTable(NVIC_VectTab_RAM, 0x0);  
-#else
-  NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x0);    
-#endif 
-} 
-
-void Delay(vu32 nCount) 
+void Delay(uint32_t nCount) 
 { 
   for(; nCount != 0; nCount--); 
 }
 
-#endif
-
+#else
 
 void ADC_Task(void const * argument)
 {
-  while(1)
-  {
+  while(1) {
     PRINTF("ADC is Working...\r\n");
     osDelay(1000);
   }
 }
+
+#endif
