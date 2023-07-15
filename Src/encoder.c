3bf3e1d4ed0a45148ef35241ff40b67df4bfed4a
wangyu <ouu_cn@163.com>
2023-07-16 02:32:34 +0800

    The processing of the encoder has been improved. 1. The encoder interrupt counter value has been converted according to the mileage wheel and encoder parameters. 2. The judgment logic for positive and negative rotation has also been increased.

--- Src/encoder.c
+++ Src/encoder.c
@@ -15,19 +15,44 @@
 #include "stm32f4xx_hal.h"
 
 /* Private macro -------------------------------------------------------------*/
+#define TESTER_TRIGGER_DISTANCE   125.0f  // Unit : mm
+#define MILEAGE_WHEEL_DIAMETER    63.66f  // Unit : mm
+#define ENCODER_PULSE_RATE        200     // Unit : pulse/r (Omron E6B2)
+#define ENCODER_MULTI_FREQ        4       // A & B signal, rising & falling edge
+#define PI                        3.1415926f
 
-/* External Variables --------------------------------------------------------*/
+/* Private type definitions --------------------------------------------------*/
+typedef enum {
+  FORWARD = 0,
+  BACKWARD
+} DIRECTION;
+
+/* Private variables ---------------------------------------------------------*/
+static DIRECTION currentDirection, previousDirection;
+static uint32_t counter = (uint32_t)(TESTER_TRIGGER_DISTANCE * 
+  ENCODER_MULTI_FREQ * ENCODER_PULSE_RATE / MILEAGE_WHEEL_DIAMETER / PI);
+
+/* External variables --------------------------------------------------------*/
 extern TIM_HandleTypeDef htim1;
 extern osSemaphoreId EncoderArriveSemHandle;
 
-/* Private Variables ---------------------------------------------------------*/
-
 /* Private function prototypes -----------------------------------------------*/
+static void initEncoder(void);
 
 /* Formal function definitions -----------------------------------------------*/
+static void initEncoder(void)
+{
+  // initialize direction flag
+  currentDirection = (__HAL_TIM_IS_TIM_COUNTING_DOWN(&htim1)) ? BACKWARD : FORWARD;
+  previousDirection = currentDirection;
+  
+  // reset encoder counter
+  __HAL_TIM_SET_AUTORELOAD(&htim1, counter - 1);
+}
+
 void startEncoder(void)
 {
-  __HAL_TIM_SET_AUTORELOAD(&htim1, 0x1234); // reset encoder counter
+  initEncoder();
   
   __HAL_TIM_CLEAR_IT(&htim1, TIM_IT_UPDATE);
   HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_ALL);
@@ -36,5 +61,9 @@ void startEncoder(void)
 
 void encoderCallback(void)
 {
-  osSemaphoreRelease(EncoderArriveSemHandle);
+  currentDirection = (__HAL_TIM_IS_TIM_COUNTING_DOWN(&htim1)) ? BACKWARD : FORWARD;
+  if (currentDirection == previousDirection) {
+    osSemaphoreRelease(EncoderArriveSemHandle);
+  }
+  previousDirection = currentDirection;
 }
