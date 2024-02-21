200a8e6ece1debd5b2d9e19ddc6883f55eab9696
wangyu <ouu_cn@163.com>
2024-02-22 07:07:26 +0800

    Modify assert output log.

--- Src/main.c
+++ Src/main.c
@@ -906,7 +906,7 @@ void assert_failed(uint8_t* file, uint32_t line)
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	printf("Wrong parameters value: file %s on line %d.\r\n", file, (int)line);
+	printf("Wrong parameters value : file %s on line %d.\r\n", file, (int)line);
 	while(1);
   /* USER CODE END 6 */
 }
